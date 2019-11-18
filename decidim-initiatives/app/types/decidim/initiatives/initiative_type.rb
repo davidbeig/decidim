aabcfda1d0197611ecd9a50f1fcb129d88fd75ef
David Igón <david.igon@riseup.net>
2019-11-18 15:14:27 +0100

    Add fields for initiative type and participatory space query type

---
+++ decidim-initiatives/app/types/decidim/initiatives/initiative_type.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Initiatives
+    # This type represents a Initiative.
+    InitiativeType = GraphQL::ObjectType.define do
+      interfaces [
+        -> { Decidim::Core::ParticipatorySpaceInterface },
+        -> { Decidim::Core::ScopableInterface },
+        -> { Decidim::Core::AttachableInterface },
+        -> { Decidim::Core::AuthorInterface}
+      ]
+
+      name "Initiatives"
+      description "A initiative"
+
+      field :description, Decidim::Core::TranslatedFieldType, "The description of this initiative."
+      field :slug, !types.String
+      field :hashtag, types.String, "The hashtag for this initiative"
+      field :createdAt, !Decidim::Core::DateTimeType, "The time this initiative was created", property: :created_at
+      field :updatedAt, !Decidim::Core::DateTimeType, "The time this initiative was updated", property: :updated_at
+      field :publishedAt, !Decidim::Core::DateTimeType, "The time this initiative was published", property: :published_at
+      field :reference, !types.String, "Reference prefix for this initiative"
+      field :state, types.String, "State of the initiative"
+      field :signatureType, types.String, "Signature type of the initiative", property: :signature_type
+      field :signatureStartDate, !Decidim::Core::DateTimeType, "The signature start date", property: :signature_start_date
+      field :signatureEndDate, !Decidim::Core::DateTimeType, "The signature end date", property: :signature_end_date
+      field :offlineVotes, types.Int, "The number of offline votes in this initiative", property: :offline_votes
+      field :initiativeVotesCount, types.Int, "The number of votes in this initiative", property: :initiative_votes_count
+      field :initiativeSupportsCount, types.Int, "The number of supports in this initiative", property: :initiative_supports_count
+
+      field :author, !Decidim::Core::AuthorInterface, "The initiative author" do
+        resolve lambda { |obj, _args, _ctx|
+          obj.user_group || obj.author
+        }      
+      end
+      
+      # These fields may be private:
+      # decidim_participatory_process_group_id
+      # decidim_organization_id
+      # private_space
+    end
+  end
+end
