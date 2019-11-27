6dcb4f360507967692d9f09247f5ec8db48333c3
David Igón <david.igon@riseup.net>
2019-11-27 14:34:21 +0100

    add conference speaker type

---
+++ decidim-conferences/app/types/decidim/conferences/conference_speaker_type.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Conferences
+    # This type represents a conference.
+    ConferenceSpeakerType = GraphQL::ObjectType.define do
+
+      name "ConferenceSpeakerType"
+      description "A conference speaker"
+
+
+      field :id, types.Int, ""
+      field :fullName, types.String, "", property: :full_name
+      field :position, Decidim::Core::TranslatedFieldType, ""
+      field :affiliation, Decidim::Core::TranslatedFieldType, ""
+      field :twitterHandle, types.String, "", property: :twitter_handle
+      field :shortBio, Decidim::Core::TranslatedFieldType, "", property: :short_bio
+      field :personalUrl, types.String, "", property: :personal_url
+      field :avatar, types.String, "" 
+      field :user, Decidim::Core::UserType do
+        resolve ->(obj, _, _) { Decidim::User.find_by(id: obj.decidim_user_id) }
+      end 
+
+      field :createdAt, Decidim::Core::DateTimeType, "", property: :created_at
+      field :updatedAt, Decidim::Core::DateTimeType, "", property: :updated_at
+
+      # These fields may be private:
+      # decidim_participatory_process_group_id
+      # decidim_organization_id
+      # private_space
+    end
+  end
+end
