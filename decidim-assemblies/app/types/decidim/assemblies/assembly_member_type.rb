10b7b232ec126cd3d6c26da96f9be345d6a6345e
David Igón <david.igon@riseup.net>
2019-11-29 16:50:26 +0100

    add area and areatype in assemblytype

--- decidim-assemblies/app/types/decidim/assemblies/assembly_member_type.rb
+++ decidim-assemblies/app/types/decidim/assemblies/assembly_member_type.rb
@@ -7,30 +7,23 @@ module Decidim
       name "AssemblyMember"
       description "An assembly member"
 
-      field :id, types.Int, ""
-      field :fullName, types.String, "", property: :full_name
-      field :position, types.String, ""
+      field :id, types.Int, "Internal ID of the member"
+      field :fullName, types.String, "Full name of the member", property: :full_name
+      field :position, types.String, "Position of the member in the assembly"
 
-      field :user, Decidim::Core::UserType do
-        resolve ->(obj, _, _) { Decidim::User.find_by(id: obj.decidim_user_id) }
-      end
+      field :user, Decidim::Core::UserType, "The corresponding decidim user", property: :user
 
-      field :createdAt, Decidim::Core::DateTimeType, "", property: :created_at
-      field :updatedAt, Decidim::Core::DateTimeType, "", property: :updated_at
+      field :createdAt, Decidim::Core::DateTimeType, "The time this member was created", property: :created_at
+      field :updatedAt, Decidim::Core::DateTimeType, "The time this member was updated", property: :updated_at
 
-      field :weight, types.Int, ""
-      field :gender, types.String, ""
-      field :birthday, Decidim::Core::DateType, ""
-      field :birthplace, types.String, ""
-      field :designationDate, Decidim::Core::DateType, "", property: :designation_date
-      field :designationMode, types.String, "", property: :designation_mode
-      field :positionOther, types.String, "", property: :position_other
-      field :ceasedDate, Decidim::Core::DateType, "", property: :ceased_date
-
-      # These fields may be private:
-      # decidim_participatory_process_group_id
-      # decidim_organization_id
-      # private_space
+      field :weight, types.Int, "Order of appearance in which it should be represented"
+      field :gender, types.String, "Gender of the member"
+      field :birthday, Decidim::Core::DateType, "Birthday date of the member"
+      field :birthplace, types.String, "Birthplace of the member"
+      field :designationDate, Decidim::Core::DateType, "Date of designation of the member", property: :designation_date
+      field :designationMode, types.String, "Mode in which the member was designated", property: :designation_mode
+      field :positionOther, types.String, "Custom position name", property: :position_other
+      field :ceasedDate, Decidim::Core::DateType, "Date of cease for the member", property: :ceased_date
     end
   end
 end
