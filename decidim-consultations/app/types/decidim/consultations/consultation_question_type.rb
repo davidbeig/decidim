af89c3e413009a009b6c15d17de4c0bc2762e6fe
David Igón <david.igon@riseup.net>
2019-11-29 18:24:51 +0100

    add description for fields in decidim-consultations types

--- decidim-consultations/app/types/decidim/consultations/consultation_question_type.rb
+++ decidim-consultations/app/types/decidim/consultations/consultation_question_type.rb
@@ -13,13 +13,13 @@ module Decidim
       name "ConsultationQuestion"
       description "A consultation question"
 
-      field :id, !types.ID, ""
-      field :title, Decidim::Core::TranslatedFieldType, ""
+      field :id, !types.ID, "Internal ID of the question"
+      field :title, Decidim::Core::TranslatedFieldType, "Title of the question"
       field :subtitle, Decidim::Core::TranslatedFieldType, "The subtitle of this question"
-      field :slug, !types.String
+      field :slug, !types.String, "Slug of the question"
       field :createdAt, !Decidim::Core::DateTimeType, "The time this question was created", property: :created_at
       field :updatedAt, !Decidim::Core::DateTimeType, "The time this question was updated", property: :updated_at
-      field :publishedAt, !Decidim::Core::DateTimeType, "The time this question was updated", property: :published_at
+      field :publishedAt, !Decidim::Core::DateTimeType, "The time this question was published", property: :published_at
 
       field :components, types[Decidim::Core::ComponentInterface] do
         description "Lists the components this space contains."
@@ -32,31 +32,26 @@ module Decidim
       end
 
       field :bannerImage, types.String, "The banner image for this question", property: :banner_image
-      field :heroImage, types.String, "", property: :hero_image
+      field :heroImage, types.String, "The hero image for this question", property: :hero_image
 
-      field :whatIsDecided, Decidim::Core::TranslatedFieldType, "", property: :what_is_decided
-      field :promoterGroup, Decidim::Core::TranslatedFieldType, "", property: :promoter_group
-      field :participatoryScope, Decidim::Core::TranslatedFieldType, "", property: :participatory_scope
-      field :questionContext, Decidim::Core::TranslatedFieldType, "", property: :question_context
-      field :reference, types.String, "", property: :reference
-      field :hashtag, types.String, "", property: :hashtag
-      field :votesCount, types.Int, "", property: :votes_count
-      field :originScope, Decidim::Core::TranslatedFieldType, "", property: :origin_scope
-      field :originTitle, Decidim::Core::TranslatedFieldType, "", property: :origin_title
-      field :originUrl, types.String, "", property: :origin_url
-      field :iFrameUrl, types.String, "", property: :i_frame_url
-      field :externalVoting, types.Boolean, "", property: :external_voting
-      field :responsesCount, types.Int, "", property: :responses_count
-      field :order, types.Int, "", property: :order
-      field :maxVotes, types.Int, "", property: :max_votes
-      field :minVotes, types.Int, "", property: :min_votes
-      field :responseGroupsCount, types.Int, "", property: :response_groups_count
-      field :instructions, Decidim::Core::TranslatedFieldType, "", property: :instructions
-
-      # These fields may be private:
-      # decidim_participatory_process_group_id
-      # decidim_organization_id
-      # private_space
+      field :whatIsDecided, Decidim::Core::TranslatedFieldType, "What is decided in this question", property: :what_is_decided
+      field :promoterGroup, Decidim::Core::TranslatedFieldType, "The promoter group of this question", property: :promoter_group
+      field :participatoryScope, Decidim::Core::TranslatedFieldType, "The participatory scope of this question", property: :participatory_scope
+      field :questionContext, Decidim::Core::TranslatedFieldType, "The context for this question", property: :question_context
+      field :reference, types.String, "The reference for this question", property: :reference
+      field :hashtag, types.String, "The hashtag of this question", property: :hashtag
+      field :votesCount, types.Int, "The number of votes in this question", property: :votes_count
+      field :originScope, Decidim::Core::TranslatedFieldType, "The origin scope of this question", property: :origin_scope
+      field :originTitle, Decidim::Core::TranslatedFieldType, "The origin title of this question", property: :origin_title
+      field :originUrl, types.String, "The origin URL for this question", property: :origin_url
+      field :iFrameUrl, types.String, "The iframe URL for this question", property: :i_frame_url
+      field :externalVoting, types.Boolean, "If the question has external voting", property: :external_voting
+      field :responsesCount, types.Int, "The number of responses for this question", property: :responses_count
+      field :order, types.Int, "The order in which the question should be represented", property: :order
+      field :maxVotes, types.Int, "The maximum number of votes in this question", property: :max_votes
+      field :minVotes, types.Int, "The minimum number of votes in this question", property: :min_votes
+      field :responseGroupsCount, types.Int, "The number of group responses for this question", property: :response_groups_count
+      field :instructions, Decidim::Core::TranslatedFieldType, "Instructions for this question", property: :instructions
     end
   end
 end
